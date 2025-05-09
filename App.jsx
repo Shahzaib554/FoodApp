cdd2224aa1daf895ccaa67f23e22ad726dcd2955
Shahzaib554 <86713644+Shahzaib554@users.noreply.github.com>
2025-05-09 14:37:46 -0700

    Testing the text

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import { View, Text} from 'react-native';
 export const App = () => {
   return (
     <View>
-      <Text style = {{fontSize: 30}}>React App</Text>
+      <Text style = {{fontSize: 30}}>React App New</Text>
     </View>
   );
 };
