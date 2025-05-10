8f5c30316b0487637cd67d5156fddc3cd01dad2e
Shahzaib554 <86713644+Shahzaib554@users.noreply.github.com>
2025-05-10 14:04:42 -0700

    Added Folder's

--- App.jsx
+++ App.jsx
@@ -5,6 +5,7 @@ export const App = () => {
   return (
     <View>
       <Text style = {{fontSize: 30}}>React App New</Text>
+      <Text style = {{fontSize: 20}}>Hello World</Text>
     </View>
   );
 };
